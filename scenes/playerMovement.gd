6ec57f3708f71ddc328eb3f95978085c555019c6
Harmen <harmen.vanderveken@hotmail.com>
2025-09-19 23:10:07 +0200

    It bumps

--- scenes/playerMovement.gd
+++ scenes/playerMovement.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-@export var speed: float = 200.0
+@export var speed: float = 350.0
 
 func _physics_process(delta: float) -> void:
 	var mouse_position = get_global_mouse_position()
@@ -8,3 +8,19 @@ func _physics_process(delta: float) -> void:
 	
 	velocity = direction * speed
 	move_and_slide()
+	
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		print("Collision:", collision)
+		
+		if collision:
+			var collider = collision.get_collider()
+
+			if collider is StaticBody2D:
+				print("Collided with:", collider.name)
+				scale = Vector2(1.5, 1.5)
+			
+				await get_tree().create_timer(0.1).timeout
+				scale = Vector2(1, 1)
+		
+		
