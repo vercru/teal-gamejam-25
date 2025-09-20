0a4d000e4fbf6353eaef7aa246ca2fcb5bb2dda3
francis <francis.vercruyssen@gmail.com>
2025-09-20 09:56:01 +0200

    sprite boop scaling optimization, renamed scenes and nodes to make it more clear

--- scenes/playerMovement.gd
+++ scenes/playerMovement.gd
@@ -2,13 +2,27 @@ extends CharacterBody2D
 
 @export var speed: float = 350.0
 @export var move_deadzone: float = 100.0
+@export var sprite: Sprite2D
+@export var collideScale: float = 1.5
+@export var collideScaleAnimationTime: float = 0.1
+
+var spriteOrigScale: Vector2
+var scaleTimer: float = 0
+
+func _ready() -> void:
+	spriteOrigScale = sprite.scale
+
+func _process(delta: float) -> void:
+	if (scaleTimer > 0):
+		scaleTimer = maxf(0, scaleTimer - delta)
+	sprite.scale = spriteOrigScale * lerp_angle(1, collideScale, scaleTimer / collideScaleAnimationTime)
 
 func _physics_process(delta: float) -> void:
 	var mouse_position = get_global_mouse_position()
 	var global_to_mouse = (mouse_position - global_position);
 
 	if global_to_mouse.length_squared() > move_deadzone:
-		var direction = (mouse_position - global_position).normalized()
+		var direction = global_to_mouse.normalized()
 
 		velocity = direction * speed
 		move_and_slide()
@@ -22,9 +36,6 @@ func _physics_process(delta: float) -> void:
 
 			if collider is StaticBody2D:
 				print("Collided with:", collider.name)
-				scale = Vector2(1.5, 1.5)
-			
-				await get_tree().create_timer(0.1).timeout
-				scale = Vector2(1, 1)
+				scaleTimer = collideScaleAnimationTime
 		
 		
