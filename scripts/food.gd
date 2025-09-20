b779ab9ba596a7ecaf9ab9a95dec55931177e7e8
francis <francis.vercruyssen@gmail.com>
2025-09-20 16:29:29 +0200

    add food spawner

--- scripts/food.gd
+++ scripts/food.gd
@@ -4,9 +4,12 @@ extends Node2D
 @export var glow: Sprite2D
 @export var minRotSpeed: float = 0.2
 @export var maxRotSpeed: float = 2.0
+@export var moveSpeed: float = 200.0
 
 var rotSpeed: float
 var forward: Vector2
+var cameraRect: Rect2
+var spawnTimer = 1
 
 func _ready() -> void:
 	var colour = Color(randf_range(0.5, 1.0), randf_range(0.5, 1.0), randf_range(0.5, 1.0))
@@ -14,9 +17,12 @@ func _ready() -> void:
 	glow.modulate = colour
 	sprite.scale *= randf_range(0.8, 1.2)
 	rotSpeed = randf_range(minRotSpeed, maxRotSpeed) * (-1 if randi() % 2 == 0 else 1)
-	var forwardAngle = randf() * 2 * PI
-	forward = Vector2(cos(forwardAngle), sin(forwardAngle))
+	forward = (-position.rotated((randf() * 2 - 1) * PI / 2)).normalized()
+	var camera_size = get_viewport_rect().size * get_viewport().get_camera_2d().zoom
+	cameraRect = Rect2(- camera_size / 2 * 1.1, camera_size * 1.1)
 
 func _process(delta: float) -> void:
 	sprite.rotate(rotSpeed * delta)
-	position += forward * delta * 10
+	position += forward * delta * moveSpeed
+	if (!cameraRect.has_point(position)):
+		queue_free()
--- scripts/foodSpawner.gd
+++ scripts/foodSpawner.gd
@@ -1,3 +1,21 @@
 extends Node2D
 
 @export var foodScene: PackedScene
+@export var spawnTime: float = 2.0
+
+var cameraRect: Rect2
+var spawnTimer: float = 0
+
+func _ready() -> void:
+	var camera_size = get_viewport_rect().size * get_viewport().get_camera_2d().zoom
+	cameraRect = Rect2(- camera_size / 2, camera_size)
+
+func _process(delta: float) -> void:
+	spawnTimer -= delta
+	if (spawnTimer < 0):
+		spawnTimer += spawnTime
+		var food: Node2D = foodScene.instantiate()
+		food.position = Vector2(
+			cameraRect.position.x + cameraRect.size.x / 2 + (randf() * 2 - 1) * cameraRect.size.x * 0.4,
+			cameraRect.position.y + cameraRect.size.y / 2 + (randf() * 2 - 1) * cameraRect.size.y * 0.4)
+		add_child(food)
